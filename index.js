e44dd91a187545d20548082726af3b1b30d3d0f3
Nabil <messaoudnabil9@gmail.com>
2021-06-06 23:39:34 +0100

    first commit6

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 //configuration
 const app = express();
 app.use(cors());
+const port = process.env.PORT || 5000;
 
+app.use(express.static(__dirname));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "home.html"));
 });
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
